871a0fa129e468f8f5fe0035289e5fbb75339500
aliccce <aliceg@outlook.com>
2016-03-16 18:59:36 +0100

    uses BesteResultat

--- treningsdagbok/src/treningsdagbok/Main.java
+++ treningsdagbok/src/treningsdagbok/Main.java
@@ -2,6 +2,7 @@ package treningsdagbok;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Scanner;
 
 public class Main {
 	Connection myCon;
@@ -15,25 +16,33 @@ public class Main {
 			db = new DatabaseConnection();
 			myCon = DriverManager.getConnection(db.getDatabase(), db.getUser(), db.getPw());
 			tui = new TUI(myCon);
-			tui.run();
 		} catch (Exception e){
 			e.printStackTrace();
 		}
 	}
 	
 	public void run() throws Exception{
+		Scanner scanner = new Scanner(System.in);
 		tui.run();
+		System.out.println("Vil du gjøre noe annet? ja/nei");
+		String svar = scanner.nextLine();
+		if (svar.toLowerCase().equals("ja")){
+			this.run();
+		} else{
+			scanner.close();
+			System.out.println("Programmet avsluttes.");
+		}
 	}
 	
 	public static void main(String[] args) {
 		Main m = new Main();
 		m.init();
-		/*
+		
 		try{
 			m.run();
 		} catch(Exception e){
 			e.printStackTrace();
-		}*/
+		}
 	}
 
 }
