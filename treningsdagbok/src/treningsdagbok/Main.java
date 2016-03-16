f6b38cda2b633a80dbf48ca42859c4a63880df50
aliccce <aliceg@outlook.com>
2016-03-16 16:58:31 +0100

    main method, this is where it all starts

--- treningsdagbok/src/treningsdagbok/Main.java
+++ treningsdagbok/src/treningsdagbok/Main.java
@@ -2,21 +2,18 @@ package treningsdagbok;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import treningsdagbok.Treningsøkt;
-import treningsdagbok.TUI;
 
 public class Main {
 	Connection myCon;
-	Treningsøkt tr;
 	TUI tui;
+	DatabaseConnection db;
 	
 	public void init(){
 		try {
 			// Activate driver and create connection with database.
 			Class.forName("com.mysql.jdbc.Driver");
-			DatabaseConnection db = new DatabaseConnection();
+			db = new DatabaseConnection();
 			myCon = DriverManager.getConnection(db.getDatabase(), db.getUser(), db.getPw());
-			tr = new Treningsøkt(myCon);
 			tui = new TUI(myCon);
 			
 		} catch (Exception e){
@@ -25,29 +22,7 @@ public class Main {
 	}
 	
 	public void run() throws Exception{
-		
-		tr.setGeneral("2016-03-20", "17:00:00", 2, 8, 8);
-		tr.setUtendørs(20, "ganske dårlig");
-		tr.setMal("favoritt");
-		tr.addØvelse(10002);
-		tr.addØvelse(10003);
-		tr.addØvelse(10004);
-		tr.addGruppe(20000);
-		tr.addNotat("Form", "Formen var velidg bra idag blabla");
-		tr.addResultat("Tid", 100, 10003);
-		tr.addResultat("Distanse", 1600, 10004);
-		
-		try{
-			//tr.fillDatabase();
-			tr.printØvelser();
-		} catch(Exception e){
-			e.printStackTrace();
-			//tr.regret();
-		}
-		
-		
-		//tui.run();
-		
+		tui.run();
 	}
 	
 	public static void main(String[] args) {
